0655a9b5f78ecf66c19cf7a452b9652b09886c67
Lobato <joao.lopes.lobato@gmail.com>
2026-03-18 12:38:58 +0000

    Itens

--- src/main/java/net/lobato/testemod/TesteMod.java
+++ src/main/java/net/lobato/testemod/TesteMod.java
@@ -1,6 +1,8 @@
 package net.lobato.testemod;
 
 import com.mojang.logging.LogUtils;
+import net.lobato.testemod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -19,7 +21,7 @@ import org.slf4j.Logger;
 public class TesteMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "testingmod";
+    public static final String MOD_ID = "testemod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -30,6 +32,8 @@ public class TesteMod
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -44,7 +48,10 @@ public class TesteMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
